089ae9fdb2141d93124180d1d071363bfee05873
Jose Luis Figueroa de Lara <figueroadelara@outlook.com>
2025-01-26 15:53:50 -0600

    Create the usart_cfg.h header file.
    This is the header file for the definition of the USART setting.
    
    Define the USART ports total number and the USART port to be used.

---
+++ FirmwareCode/include/usart_cfg.h
@@ -0,0 +1,32 @@
+/**
+ * @file usart_cfg.h
+ * @author Jose Luis Figueroa 
+ * @brief This module contains interface definitions for the USART
+ * configuration. This is the header file for the definition of the
+ * interface for retrieving the Universal Asynchronous Receiver
+ * Transmitter configuration table.
+ * @version 1.0
+ * @date 2025-01-26
+ * 
+ * @copyright Copyright (c) 2023 Jose Luis Figueroa. MIT License.
+ * 
+ */
+#ifndef USART_CFG_H_
+#define USART_CFG_H_
+
+/*****************************************************************************
+ * Includes
+******************************************************************************/
+
+/*****************************************************************************
+ * Preprocessor Constants
+******************************************************************************/
+/**
+ * Defines the number of USART peripherals on the processor.
+*/
+#define USART_PORTS_NUMBER 3U
+
+/** 
+ * Set the value according with the number of USART peripheral
+*/
+#define USART_PORTS_NUMBER 1U
