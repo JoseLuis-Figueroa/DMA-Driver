31095b6e53bd752bd4c5354d91988d6a4261dd64
Jose Luis Figueroa de Lara <figueroadelara@outlook.com>
2025-01-30 10:41:35 -0600

    Add: define the DMA_Init(), and DMA_TransferConfig() functions.
    The DMA_init initializes the DMA peripheral, and the DMA_TransferConfig() configures the transfer.

--- FirmwareCode/src/dma.c
+++ FirmwareCode/src/dma.c
@@ -110,3 +110,297 @@ static uint32_t volatile * const streamNumberOfData[DMA_PORTS_NUMBER] =
 
 /* Defines the error code flag */
 static volatile uint16_t errorCodeFlag = DMA_ERROR_CODE_NONE; /**< Error code flag*/
+
+/*****************************************************************************
+* Function Prototypes
+*****************************************************************************/
+
+/*****************************************************************************
+ * Function Definitions
+*****************************************************************************/
+/*****************************************************************************
+ * Function: DMA_init()
+ *//**
+ * \b Description:
+ * This function is used to initialize the DMA peripheral based on the
+ * configuration table defined in dma_cfg module.
+ * 
+ * PRE-CONDITION: The MCU clocks must be configured and enabled.
+ * PRE-CONDITION: The DMA_PORTS_NUMBER > 0 
+ * PRE-CONDITION: The configuration table must be populated with valid values.
+ * 
+ * POST-CONDITION: The DMA peripheral is set up with the configuration
+ * table.
+ * 
+ * @param[in]   Config is a pointer to the configuration table that contains
+ * the initialization for the peripheral.
+ * 
+ * @return void
+ * 
+ * \b Example:
+ * @code
+ * const DmaConfig_t * const DmaConfig = Dma_configGet();
+ * DMA_Init(DmaConfig);
+ * @endcode
+ * 
+ * @see DMA_configGet
+ * @see DMA_Init
+ * @see DMA_transferConfig
+ * 
+ */
+void DMA_init(const DmaConfig_t * const Config)
+{
+    /* Loop through all the elements of the configuration table. */
+    for(uint8_t i=0; i<DMA_USED_PORTS; i++)
+    {
+        /*Review if the DMA stream is correct*/
+        if(Config[i].Stream >= DMA_STREAM_MAX)
+        {
+            errorCodeFlag = DMA_ERROR_CODE_STREAM;
+            assert(errorCodeFlag != DMA_ERROR_CODE_STREAM);
+        }
+
+        /* Set the configuration of the DMA on the control register */
+        /* Set the channel of the stream*/
+        if(Config[i].Channel == DMA_CHANNEL_0)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_1)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_2)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_3)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_4)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_5)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_6)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_2;
+        }
+        else if(Config[i].Channel == DMA_CHANNEL_7)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_1;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_CHSEL_2;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_CHANNEL;
+            assert(errorCodeFlag != DMA_ERROR_CODE_CHANNEL);
+        }
+        
+        /* Set the direction of the stream*/
+        if(Config[i].Direction == DMA_PERIPHERAL_TO_MEMORY)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_DIR_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_DIR_1;
+        }
+        else if(Config[i].Direction == DMA_MEMORY_TO_PERIPHERAL)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_DIR_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_DIR_1;
+        }
+        else if(Config[i].Direction == DMA_MEMORY_TO_MEMORY)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_DIR_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_DIR_1;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_DIRECTION;
+            assert(errorCodeFlag != DMA_ERROR_CODE_DIRECTION);
+        }
+
+        /* Set the memory data size */
+        if(Config[i].MemorySize == DMA_MEMORY_SIZE_8)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_MSIZE_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_MSIZE_1;
+        }
+        else if(Config[i].MemorySize == DMA_MEMORY_SIZE_16)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_MSIZE_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_MSIZE_1;
+        }
+        else if(Config[i].MemorySize == DMA_MEMORY_SIZE_32)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_MSIZE_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_MSIZE_1;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_MEMORY_SIZE;
+            assert(errorCodeFlag != DMA_ERROR_CODE_MEMORY_SIZE);
+        }
+
+        /* Set the peripheral data size */
+        if(Config[i].PeripheralSize == DMA_PERIPHERAL_SIZE_8)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_PSIZE_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_PSIZE_1;
+        }
+        else if(Config[i].PeripheralSize == DMA_PERIPHERAL_SIZE_16)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_PSIZE_0;
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_PSIZE_1;
+        }
+        else if(Config[i].PeripheralSize == DMA_PERIPHERAL_SIZE_32)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_PSIZE_0;
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_PSIZE_1;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_PERIPHERAL_SIZE;
+            assert(errorCodeFlag != DMA_ERROR_CODE_PERIPHERAL_SIZE);
+        }
+
+        /* Set the memory increment mode*/
+        if(Config[i].MemoryIncrement == DMA_MEMORY_INCREMENT_DISABLED)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_MINC;
+        }
+        else if(Config[i].MemoryIncrement == DMA_MEMORY_INCREMENT_ENABLED)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_MINC;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_MEMORY_INCREMENT;
+            assert(errorCodeFlag != DMA_ERROR_CODE_MEMORY_INCREMENT);
+        }
+
+        /* Set the peripheral increment mode*/
+        if(Config[i].PeripheralIncrement == DMA_PERIPHERAL_INCREMENT_DISABLED)
+        {
+            *streamControlRegister[Config[i].Stream] &= ~DMA_SxCR_PINC;
+        }
+        else if(Config[i].PeripheralIncrement == DMA_PERIPHERAL_INCREMENT_ENABLED)
+        {
+            *streamControlRegister[Config[i].Stream] |= DMA_SxCR_PINC;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_PERIPHERAL_INCREMENT;
+            assert(errorCodeFlag != DMA_ERROR_CODE_PERIPHERAL_INCREMENT);
+        }
+
+        /* Set the configuration of the DMA on the FIFO control register */
+        /* Set the FIFO direct mode disable*/
+        if(Config[i].FifoMode == DMA_FIFO_DIRECT_MODE_DISABLED)
+        {
+            *streamFifoRegister[Config[i].Stream] |= DMA_SxFCR_DMDIS;
+        }
+        else if(Config[i].FifoMode == DMA_FIFO_DIRECT_MODE_ENABLED)
+        {
+            *streamFifoRegister[Config[i].Stream] &= ~DMA_SxFCR_DMDIS;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_FIFO_MODE;
+            assert(errorCodeFlag != DMA_ERROR_CODE_FIFO_MODE);
+        }
+
+        /* Set the FIFO threshold level*/
+        if(Config[i].FifoThreshold == DMA_FIFO_THRESHOLD_1_4)
+        {
+            *streamFifoRegister[Config[i].Stream] &= ~DMA_SxFCR_FTH_0;
+            *streamFifoRegister[Config[i].Stream] &= ~DMA_SxFCR_FTH_1;
+        }
+        else if(Config[i].FifoThreshold == DMA_FIFO_THRESHOLD_1_2)
+        {
+            *streamFifoRegister[Config[i].Stream] |= DMA_SxFCR_FTH_0;
+            *streamFifoRegister[Config[i].Stream] &= ~DMA_SxFCR_FTH_1;
+        }
+        else if(Config[i].FifoThreshold == DMA_FIFO_THRESHOLD_3_4)
+        {
+            *streamFifoRegister[Config[i].Stream] &= ~DMA_SxFCR_FTH_0;
+            *streamFifoRegister[Config[i].Stream] |= DMA_SxFCR_FTH_1;
+        }
+        else if(Config[i].FifoThreshold == DMA_FIFO_THRESHOLD_FULL)
+        {
+            *streamFifoRegister[Config[i].Stream] |= DMA_SxFCR_FTH_0;
+            *streamFifoRegister[Config[i].Stream] |= DMA_SxFCR_FTH_1;
+        }
+        else
+        {
+            errorCodeFlag = DMA_ERROR_CODE_FIFO_THRESHOLD;
+            assert(errorCodeFlag != DMA_ERROR_CODE_FIFO_THRESHOLD);
+        }
+
+    }
+
+}
+
+/*****************************************************************************
+ * Function: DMA_transferConfig()
+ *//**
+ * \b Description:
+ * This function is used to configure the DMA peripheral for a transfer.
+ * 
+ * PRE-CONDITION: The DMA peripheral must be initialized.
+ * PRE-CONDITION: The DMA_PORTS_NUMBER > 0 
+ * 
+ * POST-CONDITION: The DMA peripheral is set up for a transfer.
+ * 
+ * @param[in]   Stream is the DMA stream to configure.
+ * @param[in]   peripheral is the register of the peripheral.
+ * @param[in]   memory is the space of the memory.
+ * @param[in]   length is the number of data to transfer.
+ * 
+ * @return void
+ * 
+ * \b Example:
+ * @code
+ * const DmaConfig_t * const DmaConfig = Dma_configGet();
+ * DMA_Init(DmaConfig);
+ * DMA_transferConfig(DmaConfig);
+ * @endcode
+ * 
+ * @see DMA_configGet
+ * @see DMA_Init
+ * @see DMA_transferConfig
+ * 
+ */
+void DMA_transferConfig(const DmaStream_t Stream, volatile uint32_t * const peripheral,
+const uint32_t * memory, const uint32_t length)
+{
+    /* Set the memory address */
+    *streamMemory0Address[Stream] = (uint32_t)memory;
+
+    /* Set the peripheral address */
+    *streamPeripheralAddress[Stream] = (uint32_t)&peripheral;
+
+    /* Set the number of data */
+    *streamNumberOfData[Stream] = length;
+
+    /* Enable the stream */
+    *streamControlRegister[Stream] |= DMA_SxCR_EN;
+}
